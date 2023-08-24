a0e8b95dffe441368e3d64b6bd68a9a58d67782b
Alan Binu <alanbinu5@mail.com>
2023-08-24 12:19:03 +0530

    modifued experience section

--- src/containers/experienceAccordion/ExperienceAccordion.js
+++ src/containers/experienceAccordion/ExperienceAccordion.js
@@ -1,10 +1,6 @@
 import React, { Component } from "react";
 import "./exp.css";
 import { Fade, Flip } from "react-reveal";
-import infosys from "./infosys.jpg";
-import indel from "./Indel.png";
-import santhisoft from "./santhisoft.png";
-import webtechsolutions from "./webtechsolutions.png";
 
 class ExperienceAccordion extends Component {
   render() {
@@ -18,7 +14,6 @@ class ExperienceAccordion extends Component {
               <h2>Work Experience</h2>
             </center>
             <br />
-            <div class="col-twelve"></div>
             <div class="timeline-wrap">
               <div class="timeline-block">
                 <div class="timeline-ico">
@@ -28,7 +23,7 @@ class ExperienceAccordion extends Component {
                 <div class="timeline-header">
                   <h3>
                     <a href="https://www.tcs.com" target="_blank">
-                      <h5 class="resume-title">TCS</h5>
+                      <h5 class="resume-title">Tata Consultancy Services</h5>
                     </a>
                   </h3>
                   <i>September 2022 - January 2023</i>
@@ -39,37 +34,27 @@ class ExperienceAccordion extends Component {
 
                 <div class="timeline-content">
                   <h3>
-                    <h5 class="resume-title">Java Develoer</h5>
+                    <h5 class="resume-title">Java Developer</h5>
                   </h3>
-
-                  <p>
-                    Client : <i>WALGREENS</i> (Second-largest pharmacy store
-                    chain in the United State)
-                    <br />
-                    <br />
-                    Worked as a Java backend developer in MWS (Mobile Web
-                    Service) team with apps that <br />
-                    build on Microservice Architecture.
-                    <br />
-                    Worked with the dev team to fix customer problems.
-                    <br />
-                    Help the team to fix and modify the cloud Azure
-                    works/problems
-                    <br />
-                    <br />
-                  </p>
-                  {/* <div class="col-lg-4">
-                <a
-                  href="https://drive.google.com/file/d/1HgJlOEPPkcZ6e_-zbNfHaSTpBsx4NxJg/view?usp=sharing"
-                  target="_blank"
-                >
-                  <div class="blog-item-wrapper">
-                    <div class="blog-item-img">
-                      <img src={infosys} alt="" width="150" height="200"/>
-                    </div>
-                  </div>
-                </a>
-                </div> */}
+                  <ul className="ulclass">
+                    <p>
+                      Client : <i>WALGREENS</i> (Second-largest pharmacy store
+                      chain in the United State)
+                      <br />
+                    </p>
+                    <li className="liclass">
+                      Worked as a Java backend developer in MWS (Mobile Web
+                      Service) team with apps that build on Microservice
+                      Architecture.
+                    </li>
+                    <li className="liclass">
+                      Worked with the dev team to fix customer problems.
+                    </li>
+                    <li className="liclass">
+                      Help the team to fix and modify the cloud Azure
+                      works/problems
+                    </li>
+                  </ul>
                 </div>
               </div>
               <div class="timeline-block">
@@ -93,94 +78,49 @@ class ExperienceAccordion extends Component {
                   <h3>
                     <h5 class="resume-title">Full Stack Engineer</h5>
                   </h3>
-
-                  <p>
-                    Client : <i>HSBC</i> (Hongkong and Shanghai Banking
-                    Corporation) 6th largest <br />
-                    multinational investment bank and financial services holding
-                    company
-                    <br />
-                    <br />
-                    Deals with Full Stack Spring with Raect for the LoanIQ
-                    Development team.
-                    <br />
-                    *Working as a Full Stack Engineer with ReactJs and Spring
-                    boot in HSBC for LoanIQ <br />
-                    UK team for Core Banking Systems with apps that build on
-                    Microservice Architecture.
-                    <br />* Organized and leaded cross-functional problem
-                    solving teams through root cause <br />
-                    identification, verification and corrective action steps
-                    including preventive actions
-                    <br />* Created a new way of log extraction all over the
-                    LoanIQ team for faster <br />
-                    development and process <br /> requirement and it is widely
-                    used across the team.
-                    <br />* Created a React project that help to deploy the
-                    Control-M jobs to live within seconds,
-                    <br /> thus increased the productivity across the team.
-                    <br />* Created a new setup to monitor Httpd server in every
-                    5 min in Control-m to fix the <br />
-                    Apache Server crash notification
-                    <br />* Successful transformed Control-m jobs fetcher
-                    application from JSP, InfluxDb stack <br />
-                    to React, MongoDB which recrease the latency and increased
-                    effectiveness of database.
-                  </p>
-
-                  {/* <div class="col-lg-4">
-                <a
-                  href="https://drive.google.com/file/d/1HgJlOEPPkcZ6e_-zbNfHaSTpBsx4NxJg/view?usp=sharing"
-                  target="_blank"
-                >
-                  <div class="blog-item-wrapper">
-                    <div class="blog-item-img">
-                      <img src={infosys} alt="" width="150" height="200"/>
-                    </div>
-                  </div>
-                </a>
-                </div> */}
-                </div>
-              </div>
-              {/* <div class="timeline-block">
-                  <div class="timeline-ico">
-                    <i class="fa fa-briefcase" aria-hidden="true"></i>
-                  </div>
-
-                  <div class="timeline-header">
-                    <h3>
-                    <a href="https://www.indelsystems.in" target="_blank"><h5 class="resume-title">Indel Systems</h5></a>
-                    </h3>
-                    <i>April 2020 - August 2020</i>
-                    <br />
-                    <i>Full Time</i>
-                    <br />
-                  </div>
-
-                  <div class="timeline-content">
-                    <h3>
-                      <h5 class="resume-title">Software Developer - Trainee</h5>
-                    </h3>
-
+                  <ul className="ulclass">
                     <p>
-                      Worked in the Java backend for Gold Loan Application
+                      Client : <i>HSBC</i> (Hongkong and Shanghai Banking
+                      Corporation) 6th largest multinational investment bank and
+                      financial services holding company
                       <br />
                     </p>
-                    <div class="col-lg-4">
-                <a
-                  href="https://drive.google.com/file/d/1HmPMcyttoXFJZbP16Xgb4iSlGKDKyN5g/view?usp=sharing"
-                  target="_blank"
-                >
-                  <div class="blog-item-wrapper">
-                    <div class="blog-item-img">
-                      <img src={indel} alt="" width="150" height="200"/>
-                    </div>
-                  </div>
-                </a>
+                    <li className="liclass">
+                      Deals with Full Stack Spring with React for the LoanIQ
+                      Development team.
+                    </li>
+                    <li className="liclass">
+                      Working as a Full Stack Engineer with ReactJs and Spring
+                      boot in HSBC for LoanIQ UK team for Core Banking Systems
+                      with apps that build on Microservice Architecture.
+                    </li>
+                    <li className="liclass">
+                      Organized and leaded cross-functional problem solving
+                      teams through root cause identification, verification and
+                      corrective action steps including preventive actions
+                    </li>
+                    <li className="liclass">
+                      Created a new way of log extraction all over the LoanIQ
+                      team for faster development and process requirement and it
+                      is widely used across the team.
+                    </li>
+                    <li className="liclass">
+                      Created a React project that help to deploy the Control-M
+                      jobs to live within seconds, thus increased the
+                      productivity across the team
+                    </li>
+                    <li className="liclass">
+                      Created a new setup to monitor Httpd server in every 5 min
+                      in Control-m to fix the Apache Server crash notification
+                    </li>
+                    <li className="liclass">
+                      Successful transformed Control-m jobs fetcher application
+                      from JSP, InfluxDb stack to React, MongoDB which recrease
+                      the latency and increased effectiveness of database.
+                    </li>
+                  </ul>
                 </div>
-                  </div>
-                </div> */}
-
+              </div>
               <div class="timeline-block">
                 <div class="timeline-block">
                   <div class="timeline-ico">
@@ -203,32 +143,21 @@ class ExperienceAccordion extends Component {
                     <h3>
                       <h5 class="resume-title">Software Developer - Intern</h5>
                     </h3>
-
-                    <p>
-                      Santhisoft is a software product development company in
-                      Kerala, India, incorporated <br />
-                      in 2005 and is managed by CMI congregation. Apart from
-                      software product development, <br />
-                      company offers customized software solutions, IT
-                      consultancy services and training to <br />
-                      their customers.
-                      <br />I worked here for 5 months for my major project
-                      which includes the technologies <br /> like Java, JSP,
-                      HTML, CSS, MySql, Bootstrap and PHP
-                      <br />
-                    </p>
-                    {/* <div class="col-lg-4">
-                <a
-                  href="https://drive.google.com/file/d/1uxuRJcwtbE48p61CzSI7XH71h4Lljgu9/view?usp=sharing"
-                  target="_blank"
-                >
-                  <div class="blog-item-wrapper">
-                    <div class="blog-item-img">
-                      <img src={santhisoft} alt="" width="150" height="200"/>
-                    </div>
-                  </div>
-                </a>
-                </div> */}
+                    <ul className="ulclass">
+                      <li className="liclass">
+                        Santhisoft is a software product development company in
+                        Kerala, India, incorporated in 2005 and is managed by
+                        CMI congregation. Apart from software product
+                        development, company offers customized software
+                        solutions, IT consultancy services and training to their
+                        customers.
+                      </li>
+                      <li className="liclass">
+                        I worked here for 5 months for my major project which
+                        includes the technologies like Java, JSP, HTML, CSS,
+                        MySql, Bootstrap and PHP
+                      </li>
+                    </ul>
                   </div>
                 </div>
 
@@ -256,22 +185,18 @@ class ExperienceAccordion extends Component {
                     <h3>
                       <h5 class="resume-title">Founder</h5>
                     </h3>
-
-                    <p>
-                      Mainly we provide Web Application with online payment and
-                      online database for <br />
-                      securing the data, System Softwares, Desktops, Laptops and
-                      we are also providing
-                      <br /> computer repare facility.
-                      <br />
-                    </p>
-                    <div class="col-lg-4">
-                      {/* <div class="blog-item-wrapper">
-                    <div class="blog-item-img">
-                      <img src={webtechsolutions} alt="" width="250" height="150"/>
-                    </div>
-                  </div> */}
-                    </div>
+                    <ul className="ulclass">
+                      <li className="liclass">
+                        Mainly we provide Web Application with online payment
+                        and online database for securing the data
+                      </li>
+                      <li className="liclass">
+                        Wholesale and retail of computer parts.
+                      </li>
+                      <li className="liclass">
+                        We are also providing computer repare facility.
+                      </li>
+                    </ul>
                   </div>
                 </div>
               </div>
